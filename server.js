dcad80dcb0493b1d795f2ae2403bfadfcde18499
sdesire <sdesire@ltu.edu>
2021-10-26 16:20:27 -0400

    404

--- server.js
+++ server.js
@@ -40,12 +40,20 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
-app.get('api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+app.get('api/animals/:id', (req, res) => {
+    const results = findById(req.params.id, animals);
+    if (results) {
+        res.json(results);
+    }   else {
+        res.send(404);
     }
-    res.json('results');
+    
+      res.json(results);
+
 });
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!');
